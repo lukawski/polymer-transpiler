462e841eae58a5b713a4cc7cc1207fb2d50a72ab
Kacper Łukawski <klukawski@ad.wayn.net>
2018-03-19 16:23:32 +0100

    Version 1 almost done.

--- create-class.js
+++ create-class.js
@@ -1,3 +1,5 @@
+const pretty = require('pretty');
+
 module.exports = (config) => {
   const mixinsTemplate = `Polymer.mixinBehaviors(${config.base.slice(1)},
     ${config.base[0]}
@@ -5,14 +7,18 @@ module.exports = (config) => {
 
   return `
     class ${config.name} extends ${config.base.length > 1 ? mixinsTemplate : config.base[0]} {
-      ${config.staticProps.map(prop => `
-        static get ${prop.name}() { return ${JSON.stringify(prop.content)} }
+      ${config.staticProps.map(prop =>  `
+        static get ${prop.name}() { return ${prop.name === 'template' ? `Polymer.html\`\n${pretty(prop.content)} \`\n` : JSON.stringify(prop.content)} }
         `).join('')}
 
       ${config.lifecycleMethods.map(method => `
         ${method.name}() {
           ${method.content}
         }
+      `).join('')}
+
+      ${config.methods.map(method => `
+        ${method.name} ${method.content}
       `).join('')}
   }
 
