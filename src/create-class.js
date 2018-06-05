d0a4dca9903837b85b4c287ebd3c521c74882f99
Marek Streich <657275+mstreich@users.noreply.github.com>
2018-06-05 23:14:21 +0200

    Doner

--- src/create-class.js
+++ src/create-class.js
@@ -6,6 +6,7 @@ module.exports = (config) => {
   )`;
 
   return `
+
     class ${config.name} extends ${config.base.length > 1 ? mixinsTemplate : config.base[0]} {
       ${config.staticProps.map(prop =>  `
         static get ${prop.name}() { return ${prop.name === 'template' ? `Polymer.html\`\n${pretty(prop.content)} \`\n` : JSON.stringify(prop.content)} }
@@ -20,6 +21,7 @@ module.exports = (config) => {
       ${config.methods.map(method => `
         ${method.name} ${method.content}
       `).join('')}
+
   }
 
   customElements.define(${config.name}.is, ${config.name});
